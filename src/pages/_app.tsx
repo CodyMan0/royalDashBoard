05d6338ecec371d72f54693b98a3b2c5c8534f45
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-01-10 19:19:00 +0900

    feat: NextSEO를 활용해 더욱 SEO 측면 향상

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Apploading from "../layout/AppLoading";
+import { DefaultSeo } from "next-seo";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -12,14 +13,22 @@ const queryClient = new QueryClient({
 	},
 });
 
+const defaultSEO = {
+	defaultTitle: "Royal-Garden",
+	description: "this is made for intouch-church",
+};
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<RecoilRoot>
-			<QueryClientProvider client={queryClient}>
-				<Apploading>
-					<Component {...pageProps} />
-				</Apploading>
-			</QueryClientProvider>
-		</RecoilRoot>
+		<>
+			<DefaultSeo {...defaultSEO} />
+			<RecoilRoot>
+				<QueryClientProvider client={queryClient}>
+					<Apploading>
+						<Component {...pageProps} />
+					</Apploading>
+				</QueryClientProvider>
+			</RecoilRoot>
+		</>
 	);
 }
